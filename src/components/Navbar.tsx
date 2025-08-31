297dc51b14c6d28623477314e3688cb8a1bb7391
arun-kumar-24 <arunkumarm.2645s@gmail.com>
2025-08-31 19:22:44 +0530

    Tried changing font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,6 +51,7 @@ const Navbar = () => {
                 className={`text-base font-medium hover:text-[var(--color-primary)] transition-colors ${
                   pathname === link.href ? "underline underline-offset-4" : ""
                 }`}
+                // style={{ fontFamily: 'Doto, sans-serif' }}
               >
                 {link.label}
               </Link>
