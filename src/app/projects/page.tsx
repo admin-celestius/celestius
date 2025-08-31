143ed7b8a7a752a7b7dbc1f55d12fcee7e92f86e
arun-kumar-24 <arunkumarm.2645s@gmail.com>
2025-08-31 17:17:49 +0530

    Dev (#6)
    * Fixed scrolling for We do and added footer
    
    * Add footer in layout
    
    * Event card sample
    
    * Event Card final change
    
    * Event Card final final change
    
    * Event Card final final final change
    
    * Projects card added

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import React from "react";
+import ProjectsList from "@/components/ProjectsList";
+
 export default function Projects() {
   return (
-    <div> Celestius Projects Page </div>
+    <main className="h-screen bg-black text-white p-8">
+      <h1 className="text-3xl font-bold text-yellow-400 mb-6">Celestius Projects</h1>
+
+      <div className="h-full overflow-y-auto">
+        <ProjectsList />
+      </div>
+    </main>
   );
 }
